8c2fdb07500212e327724d5fd324b8dcf1699c19
DOOMED2WIN <timon@codeschluss.de>
2022-12-20 12:02:20 +0100

    InformationAPI and landing titleImage

---
+++ src/main/java/app/wooportal/server/base/cms/themeVariable/ThemeVariableEntity.java
@@ -0,0 +1,27 @@
+package app.wooportal.server.base.cms.themeVariable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import app.wooportal.server.core.base.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+@Entity
+@Table(name = "theme_variables")
+@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+public class ThemeVariableEntity extends BaseEntity {
+
+  private static final long serialVersionUID = 1L;
+
+  private String key;
+
+  private String value;
+}
---
+++ src/main/java/app/wooportal/server/base/cms/themeVariable/ThemeVariableRepository.java
@@ -0,0 +1,9 @@
+package app.wooportal.server.base.cms.themeVariable;
+
+import app.wooportal.server.core.repository.DataRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ThemeVariableRepository extends DataRepository<ThemeVariableEntity> {
+
+}
