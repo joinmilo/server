4c5277e41789adf903dd7e7a608582f09813b080
vetemi <valmir@codeschluss.de>
2023-02-21 13:59:33 +0100

    little fix

--- src/main/java/app/wooportal/server/base/cms/pages/PagePredicateBuilder.java
+++ src/main/java/app/wooportal/server/base/cms/pages/PagePredicateBuilder.java
@@ -3,18 +3,17 @@ package app.wooportal.server.base.cms.pages;
 import org.springframework.stereotype.Service;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import app.wooportal.server.core.base.PredicateBuilder;
-import app.wooportal.server.features.contests.types.QContestTypeEntity;
 
 @Service
 public class PagePredicateBuilder
-    extends PredicateBuilder<QContestTypeEntity, PageEntity> {
+    extends PredicateBuilder<QPageEntity, PageEntity> {
 
   public PagePredicateBuilder() {
-    super(QContestTypeEntity.contestTypeEntity);
+    super(QPageEntity.pageEntity);
   }
 
   @Override
   public BooleanExpression freeSearch(String term) {
-    return query.key.likeIgnoreCase(term).or(query.translatables.any().name.likeIgnoreCase(term));
+    return query.slug.likeIgnoreCase(term).or(query.translatables.any().name.likeIgnoreCase(term));
   }
 }
