63d7bcf3a2333b63615d61bdbbae3213fedba410
vetemi <valmir@codeschluss.de>
2023-03-09 10:24:35 +0100

    fixed bugs

--- src/main/java/app/wooportal/server/core/context/GraphQlContextAdapter.java
+++ src/main/java/app/wooportal/server/core/context/GraphQlContextAdapter.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import graphql.language.Definition;
 import graphql.language.Field;
+import graphql.language.FragmentDefinition;
 import graphql.language.SelectionSetContainer;
+import graphql.parser.InvalidSyntaxException;
 import graphql.parser.Parser;
 import lombok.Getter;
 import lombok.Setter;
@@ -40,41 +43,56 @@ public class GraphQlContextAdapter implements ApiContextAdapter {
   }
   
   public List<Field> getSingleReadContext() {
-    var context = getReadContext();
-    if (context != null && !context.isEmpty()) {
-      return context.get(0).getSelectionSet().getSelectionsOfType(Field.class);
+    var definitions = getReadContext();
+    if (definitions == null) {
+      return null;
     }
-    return null;
+    
+    List<Field> selections = null;
+    for (var def : definitions) {
+      if (def instanceof FragmentDefinition) {
+        return fragmentContext(def);
+      } else if (def instanceof SelectionSetContainer) {
+        selections = ((SelectionSetContainer<?>) def).getSelectionSet().getSelectionsOfType(Field.class);
+      }
+    }
+    return selectionFragment(selections);
   }
-  
+
   public List<Field> getMultiReadContext() {
-    for (Field selection : getSingleReadContext()) {
+    for (var selection : getSingleReadContext()) {
       if (selection.getName().equalsIgnoreCase("result")) {
-        return selection.getSelectionSet().getSelectionsOfType(Field.class); 
+        return selection.getSelectionSet().getSelectionsOfType(Field.class);
       }
     }
     return null;
   }
   
-  public List<Field> getReadContext() {
+  @SuppressWarnings("rawtypes")
+  private List<Definition> getReadContext() {
     try {
-      var parser = new Parser();
-      var document = parser.parseDocument(getContext("query").asText());
-      for (var def : document.getDefinitions()) {
-        if (def instanceof SelectionSetContainer) {
-          return ((SelectionSetContainer<?>) def).getSelectionSet().getSelectionsOfType(Field.class);
-        }
-      }
-    } catch (IOException e) {
+      return new Parser().parseDocument(getContext("query").asText()).getDefinitions();
+    } catch (InvalidSyntaxException | IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
     return null;
   }
   
+  private List<Field> fragmentContext(Definition<?> def) {
+    return ((FragmentDefinition) def).getSelectionSet().getSelectionsOfType(Field.class);
+  }
+  
+  private List<Field> selectionFragment(List<Field> selections) {
+    if (selections != null && !selections.isEmpty()) {
+      return selections.get(0).getSelectionSet().getSelectionsOfType(Field.class);
+    }
+    return null;
+  }
+
   private JsonNode getSaveContext() {
     try {
-      return getContext("variables").fields().next().getValue(); 
+      return getContext("variables").fields().next().getValue();
     } catch (Exception ignored) { }
     return null;
   }
