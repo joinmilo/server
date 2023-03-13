6c75fb60cc30cbb699ab04228560799f32765463
DOOMED2WIN <timon@codeschluss.de>
2023-03-13 09:25:58 +0100

    updated changes

--- src/main/java/app/wooportal/server/base/feedback/translations/FeedbackTranslatableEntity.java
+++ src/main/java/app/wooportal/server/base/feedback/translations/FeedbackTranslatableEntity.java
@@ -3,7 +3,7 @@ package app.wooportal.server.base.feedback.translations;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import app.wooportal.server.base.developer.DeveloperEntity;
+import app.wooportal.server.base.feedback.base.FeedbackEntity;
 import app.wooportal.server.core.i18n.entities.TranslatableEntity;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -17,11 +17,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "feedback_translatables")
-public class FeedbackTranslatableEntity extends TranslatableEntity<DeveloperEntity> {
+public class FeedbackTranslatableEntity extends TranslatableEntity<FeedbackEntity> {
 
   private static final long serialVersionUID = 1L;
 
   @Column(nullable = false)
   private String content;
-  
+
 }
