cf9773b34c71833ac859bb7829f8b565548a4304
DOOMED2WIN <timon@codeschluss.de>
2023-06-20 15:30:23 +0200

    added labels

--- src/main/java/app/wooportal/server/features/event/rating/EventRatingEntity.java
+++ src/main/java/app/wooportal/server/features/event/rating/EventRatingEntity.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import app.wooportal.server.base.userContext.base.UserContextEntity;
 import app.wooportal.server.core.base.BaseEntity;
 import app.wooportal.server.features.event.base.EventEntity;
@@ -24,6 +25,9 @@ public class EventRatingEntity extends BaseEntity {
 
   private static final long serialVersionUID = 1L;
 
+  @Transient
+  private String captchaToken;
+  
   @Column(nullable = false)
   private Integer score;
 
