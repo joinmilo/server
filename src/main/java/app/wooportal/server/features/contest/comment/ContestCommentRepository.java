60b755b5d461ed7a18d1ffee466f1caa79eda3ca
DOOMED2WIN <timon@codeschluss.de>
2023-07-27 12:58:07 +0200

    #630 finished

---
+++ src/main/java/app/wooportal/server/features/contest/comment/ContestCommentRepository.java
@@ -0,0 +1,9 @@
+package app.wooportal.server.features.contest.comment;
+
+import org.springframework.stereotype.Repository;
+import app.wooportal.server.core.repository.DataRepository;
+
+@Repository
+public interface ContestCommentRepository extends DataRepository<ContestCommentEntity> {
+
+}
---
+++ src/main/java/app/wooportal/server/features/contest/comment/translations/ContestCommentTranslatableEntity.java
@@ -0,0 +1,27 @@
+package app.wooportal.server.features.contest.comment.translations;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import app.wooportal.server.core.i18n.entities.TranslatableEntity;
+import app.wooportal.server.features.contest.comment.ContestCommentEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+@Entity
+@Table(name = "contest_comment_translatables")
+public class ContestCommentTranslatableEntity extends TranslatableEntity<ContestCommentEntity> {
+
+  private static final long serialVersionUID = 1L;
+
+  @Column(nullable = false)
+  private String content;
+  
+}
