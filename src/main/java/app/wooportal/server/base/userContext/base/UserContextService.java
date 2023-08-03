d99d4a6af67fbd4e194c1823f9034c5da30dcb53
DOOMED2WIN <timon@codeschluss.de>
2023-08-03 09:56:53 +0200

    wip

--- src/main/java/app/wooportal/server/base/userContext/base/UserContextService.java
+++ src/main/java/app/wooportal/server/base/userContext/base/UserContextService.java
@@ -1,11 +1,9 @@
 package app.wooportal.server.base.userContext.base;
 
 import java.util.Optional;
-
 import org.springframework.stereotype.Service;
-
 import com.fasterxml.jackson.databind.JsonNode;
-
+import app.wooportal.server.base.address.base.AddressService;
 import app.wooportal.server.core.base.DataService;
 import app.wooportal.server.core.repository.DataRepository;
 import app.wooportal.server.core.security.components.user.UserService;
@@ -24,10 +22,12 @@ public class UserContextService extends DataService<UserContextEntity, UserConte
       UserContextPredicateBuilder predicate,
       AuthenticationService authService,
       SlugService slugService,
-      UserService userService) {
+      UserService userService,
+      AddressService addressService) {
     super(repo, predicate);
     
     addService("user", userService);
+    addService("address", addressService);
     
     this.authService = authService;
     this.slugService = slugService;
