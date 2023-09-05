76be4a4261e7cab4250171d494896744511089c0
DOOMED2WIN <timon@codeschluss.de>
2023-09-05 12:32:42 +0200

    wip

--- src/main/java/app/wooportal/server/base/analytics/googleSearch/SearchConsoleService.java
+++ src/main/java/app/wooportal/server/base/analytics/googleSearch/SearchConsoleService.java
@@ -14,6 +14,8 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.searchconsole.v1.SearchConsole;
+import com.google.api.services.searchconsole.v1.model.ApiDimensionFilter;
+import com.google.api.services.searchconsole.v1.model.ApiDimensionFilterGroup;
 import com.google.api.services.searchconsole.v1.model.SearchAnalyticsQueryRequest;
 import app.wooportal.server.core.base.dto.analytics.AnalyticsDto;
 import app.wooportal.server.core.config.GeneralConfiguration;
@@ -22,18 +24,21 @@ import app.wooportal.server.core.config.GeneralConfiguration;
 public class SearchConsoleService {
   
   private final GeneralConfiguration generalConfig;
-  
-  private final SearchConsoleConfig searchConfig;
+  private final SearchConsoleConfig config;
   
   private final String clicks = "clicks";
   
   private final String impressions = "impressions";
   
+  private final String ctr = "ctr";
+  
+  private final String position = "position";
+  
   public SearchConsoleService(
       SearchConsoleConfig searchConfig,
       GeneralConfiguration generalConfig) {
+    this.config = searchConfig;
     this.generalConfig = generalConfig;
-    this.searchConfig = searchConfig;
   }
   
   public SearchAnalyticsDto calculateTotal(
@@ -48,7 +53,7 @@ public class SearchConsoleService {
     var result = new SearchAnalyticsDto();
     for (var row : searchService
         .searchanalytics()
-        .query(generalConfig.getHost(), query)
+        .query(config.getHost(), query)
         .execute()
         .getRows()) {
       result.addClicks(row.getClicks().intValue());
@@ -91,6 +96,56 @@ public class SearchConsoleService {
     return result.entrySet().stream().map(e -> new AnalyticsDto(e.getKey(), e.getValue()))
         .collect(Collectors.toList());
   }
+  
+  public List<AnalyticsDto> calculateForFeature(
+      LocalDate startDate,
+      LocalDate endDate,
+      String keyword) throws IOException {
+    var searchService = createSearchService();
+    var baseQuery = new SearchAnalyticsQueryRequest()
+        .setSearchType(null)
+        .setStartDate(startDate.format(DateTimeFormatter.ISO_LOCAL_DATE))
+        .setEndDate(endDate.format(DateTimeFormatter.ISO_LOCAL_DATE))
+        .setDimensions(Arrays.asList("date"));
+    
+    var filterGroup = new ApiDimensionFilterGroup();
+    var filter = new ApiDimensionFilter();
+    filter.setDimension("page");
+    filter.setOperator("contains");
+    filter.setExpression(keyword);
+    filterGroup.setFilters(Arrays.asList(filter));
+    
+     var query = baseQuery.setDimensionFilterGroups(Arrays.asList(filterGroup));
+ 
+    var result = new HashMap<String, HashMap<String, Double>>();
+    for (var row : searchService
+        .searchanalytics()
+        .query(config.getHost(), query)
+        .execute()
+        .getRows()) {
+         
+      var entry = result.containsKey(row.getKeys().get(0))
+          ? result.get(row.getKeys().get(0))
+          : new HashMap<String, Double>();
+      entry.put(clicks, 
+          entry.containsKey(clicks) ? entry.get(clicks) + row.getClicks() : row.getClicks()); 
+      entry.put(impressions, 
+          entry.containsKey(impressions) ? entry.get(impressions) + row.getImpressions() : row.getImpressions());
+      entry.put(ctr,
+          entry.containsKey(ctr) ? entry.get(ctr) + row.getCtr() : row.getCtr());
+      entry.put(position, 
+          entry.containsKey(position) ? entry.get(position) + row.getPosition() : row.getPosition());
+      
+      result.put(row.getKeys().get(0), entry);
+  
+      row.getKeys().get(0);
+      row.getKeys().get(1);
+      
+    }
+
+    return result.entrySet().stream().map(e -> new AnalyticsDto(e.getKey(), e.getValue()))
+        .collect(Collectors.toList());
+  }
 
   public SearchConsole createSearchService() throws IOException {
 //  // As of now, this cannot be passed to SearchConsole.Builder
@@ -102,14 +157,13 @@ public class SearchConsoleService {
   // Only the deprecated method works
   @SuppressWarnings("deprecation")
   var credentials = GoogleCredential
-    .fromStream(new ClassPathResource(searchConfig.getCredentials())
+    .fromStream(new ClassPathResource(config.getCredentials())
         .getInputStream())
-    .createScoped(Collections.singletonList(searchConfig.getScope()));
+    .createScoped(Collections.singletonList(config.getScope()));
 
   return new SearchConsole.Builder(
       new NetHttpTransport(), 
       new GsonFactory(), 
       credentials).build();
   }
-  
 }
