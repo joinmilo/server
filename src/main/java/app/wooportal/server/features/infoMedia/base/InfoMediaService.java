0fd67e84136feb5e83fcc65ca8d383ad71e3c7d4
PRiedmiller <peter@codeschluss.de>
2023-10-01 10:05:04 +0200

    TG-320 #in-progress

--- src/main/java/app/wooportal/server/features/infoMedia/base/InfoMediaService.java
+++ src/main/java/app/wooportal/server/features/infoMedia/base/InfoMediaService.java
@@ -2,6 +2,7 @@ package app.wooportal.server.features.infoMedia.base;
 
 import org.springframework.stereotype.Service;
 import app.wooportal.server.core.base.DataService;
+import app.wooportal.server.core.media.base.MediaService;
 import app.wooportal.server.core.repository.DataRepository;
 
 @Service
@@ -9,8 +10,11 @@ public class InfoMediaService extends DataService<InfoMediaEntity, InfoMediaPred
 
   public InfoMediaService(
       DataRepository<InfoMediaEntity> repo,
-      InfoMediaPredicateBuilder predicate) {
+      InfoMediaPredicateBuilder predicate,
+      MediaService mediaService) {
     super(repo, predicate);
+    
+    addService("media", mediaService);
 
   }
 }
