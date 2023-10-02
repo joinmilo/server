561c6da61cb617efa69945701bacdedf9266f4d0
vetemi <valmir@codeschluss.de>
2023-10-02 19:10:53 +0200

    fixed rebase error

--- src/main/java/app/wooportal/server/features/event/base/EventService.java
+++ src/main/java/app/wooportal/server/features/event/base/EventService.java
@@ -1,29 +1,28 @@
 package app.wooportal.server.features.event.base;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import app.wooportal.server.base.address.base.AddressService;
 import app.wooportal.server.core.base.DataService;
 import app.wooportal.server.core.repository.DataRepository;
 import app.wooportal.server.features.event.attendeeConfiguration.EventAttendeeConfigurationService;
-import app.wooportal.server.features.event.schedule.EventScheduleService;
 import app.wooportal.server.features.event.media.EventMediaService;
+import app.wooportal.server.features.event.schedule.EventScheduleService;
 
 @Service
 public class EventService extends DataService<EventEntity, EventPredicateBuilder> {
 
   public EventService(DataRepository<EventEntity> repo,
       EventPredicateBuilder predicate,
-      @Lazy EventAttendeeConfigurationService attendeeConfigurationService,
+      EventAttendeeConfigurationService attendeeConfigurationService,
       AddressService addressService,
       EventScheduleService scheduleService,
-      @Lazy EventAttendeeConfigurationService attendeeConfigurationService,
       EventMediaService eventMediaService) {
     super(repo, predicate);
 
     addService("attendeeConfiguration", attendeeConfigurationService);
     addService("address", addressService);
     addService("schedules", scheduleService);
+    addService("uploads", eventMediaService);
    
   }
   
