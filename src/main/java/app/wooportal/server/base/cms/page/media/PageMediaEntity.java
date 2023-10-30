35758ae82e6f40b118ca8dff40701123dc1065d6
PRiedmiller <peter@codeschluss.de>
2023-10-30 18:28:35 +0100

    TG-614 #developed

--- src/main/java/app/wooportal/server/base/cms/page/media/PageMediaEntity.java
+++ src/main/java/app/wooportal/server/base/cms/page/media/PageMediaEntity.java
@@ -1,13 +1,11 @@
 package app.wooportal.server.base.cms.page.media;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-
 import app.wooportal.server.base.cms.page.PageEntity;
 import app.wooportal.server.core.base.BaseEntity;
 import app.wooportal.server.core.media.base.MediaEntity;
@@ -28,8 +26,7 @@ public class PageMediaEntity extends BaseEntity {
 
   private static final long serialVersionUID = 1L;
   
-  @Column(nullable = false)
-  private Boolean title;
+  private Boolean title = false;
   
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(nullable = false)
--- src/main/java/app/wooportal/server/base/cms/page/media/PageMediaService.java
+++ src/main/java/app/wooportal/server/base/cms/page/media/PageMediaService.java
@@ -1,8 +1,8 @@
 package app.wooportal.server.base.cms.page.media;
 
 import org.springframework.stereotype.Service;
-
 import app.wooportal.server.core.base.DataService;
+import app.wooportal.server.core.media.base.MediaService;
 import app.wooportal.server.core.repository.DataRepository;
 
 @Service
@@ -10,7 +10,9 @@ public class PageMediaService
     extends DataService<PageMediaEntity, PageMediaPredicateBuilder> {
 
   public PageMediaService(DataRepository<PageMediaEntity> repo,
-      PageMediaPredicateBuilder predicate) {
+      PageMediaPredicateBuilder predicate, MediaService mediaService) {
     super(repo, predicate);
+    
+    addService("media", mediaService);
   }
 }
