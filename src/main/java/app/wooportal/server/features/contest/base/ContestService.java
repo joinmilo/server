6e0114d1b9fbc92b2af88e6ac23d9df059cabc6a
doomed2code <doomed2code@doomed2code-ThinkPad-P51s>
2023-11-16 16:16:16 +0100

    added Services to contact entity

--- src/main/java/app/wooportal/server/features/contest/base/ContestService.java
+++ src/main/java/app/wooportal/server/features/contest/base/ContestService.java
@@ -1,15 +1,27 @@
 package app.wooportal.server.features.contest.base;
 
 import org.springframework.stereotype.Service;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import app.wooportal.server.base.contact.ContactService;
 import app.wooportal.server.core.base.DataService;
 import app.wooportal.server.core.repository.DataRepository;
+import app.wooportal.server.features.contest.base.media.ContestMediaService;
+
 
 @Service
 public class ContestService extends DataService<ContestEntity, ContestPredicateBuilder> {
 
-  public ContestService(DataRepository<ContestEntity> repo, ContestPredicateBuilder predicate) {
+  public ContestService(DataRepository<ContestEntity> repo, ContestPredicateBuilder predicate,
+      ContactService contactService, ContestMediaService uploads) {
     super(repo, predicate);
+    
+    addService("uploads", uploads);
+    addService("contact", contactService);
+  }
+  
+  public void preCreate(ContestEntity entity, ContestEntity newEntity, JsonNode context) {
+    newEntity.setSponsored(false);
+    addContext("sponsored", context);
   }
   
   public Boolean sponsorContest(String contestId) {
