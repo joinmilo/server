a184eafb8c295e9e790a259a71fdfa5e2931dbdc
doomed2code <doomed2code@doomed2code-ThinkPad-P51s>
2023-11-17 16:09:04 +0100

    default language german

--- src/main/java/app/wooportal/server/core/i18n/components/language/LanguageService.java
+++ src/main/java/app/wooportal/server/core/i18n/components/language/LanguageService.java
@@ -1,11 +1,13 @@
 package app.wooportal.server.core.i18n.components.language;
 
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import app.wooportal.server.base.configuration.ConfigurationService;
 import app.wooportal.server.core.base.DataService;
 import app.wooportal.server.core.i18n.TranslationsConfiguration;
 import app.wooportal.server.core.repository.DataRepository;
+import app.wooportal.server.features.article.components.media.ArticleMediaEntity;
 
 @Service
 public class LanguageService extends DataService<LanguageEntity, LanguagePredicateBuilder> {
