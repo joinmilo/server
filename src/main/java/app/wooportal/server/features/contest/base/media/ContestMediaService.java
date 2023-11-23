e27208817144c061312d2d6e2b7e5ff51b85e72f
DOOMED2WIN <timon-steinhauser@web.de>
2023-11-23 14:17:49 +0100

    contest update

--- src/main/java/app/wooportal/server/features/contest/base/media/ContestMediaService.java
+++ src/main/java/app/wooportal/server/features/contest/base/media/ContestMediaService.java
@@ -1,8 +1,8 @@
 package app.wooportal.server.features.contest.base.media;
 
 import org.springframework.stereotype.Service;
-
 import app.wooportal.server.core.base.DataService;
+import app.wooportal.server.core.media.base.MediaService;
 import app.wooportal.server.core.repository.DataRepository;
 
 @Service
@@ -10,7 +10,9 @@ public class ContestMediaService
     extends DataService<ContestMediaEntity, ContestMediaPredicateBuilder> {
 
   public ContestMediaService(DataRepository<ContestMediaEntity> repo,
-      ContestMediaPredicateBuilder predicate) {
+      ContestMediaPredicateBuilder predicate, MediaService mediaService) {
     super(repo, predicate);
+    
+    addService("media", mediaService);
   }
 }
--- src/main/java/app/wooportal/server/features/contest/participation/media/ContestParticipationMediaService.java
+++ src/main/java/app/wooportal/server/features/contest/participation/media/ContestParticipationMediaService.java
@@ -1,8 +1,8 @@
 package app.wooportal.server.features.contest.participation.media;
 
 import org.springframework.stereotype.Service;
-
 import app.wooportal.server.core.base.DataService;
+import app.wooportal.server.core.media.base.MediaService;
 import app.wooportal.server.core.repository.DataRepository;
 
 @Service
@@ -10,7 +10,9 @@ public class ContestParticipationMediaService
     extends DataService<ContestParticipationMediaEntity, ContestParticipationMediaPredicateBuilder> {
 
   public ContestParticipationMediaService(DataRepository<ContestParticipationMediaEntity> repo,
-      ContestParticipationMediaPredicateBuilder predicate) {
+      ContestParticipationMediaPredicateBuilder predicate, MediaService mediaService) {
     super(repo, predicate);
+    
+    addService("media", mediaService);
   }
 }
