01d76264f75d908c91af4244c6fb1c54d8f40e57
vetemi <valmir@codeschluss.de>
2023-11-28 09:44:03 +0100

    TG-921 - added first widgets

--- src/main/java/app/wooportal/server/base/cms/page/widget/PageWidgetService.java
+++ src/main/java/app/wooportal/server/base/cms/page/widget/PageWidgetService.java
@@ -1,7 +1,7 @@
 package app.wooportal.server.base.cms.page.widget;
 
 import org.springframework.stereotype.Service;
-
+import app.wooportal.server.base.cms.page.widgetAttribute.PageWidgetAttributeService;
 import app.wooportal.server.core.base.DataService;
 import app.wooportal.server.core.repository.DataRepository;
 
@@ -11,7 +11,10 @@ public class PageWidgetService
 
   public PageWidgetService(
       DataRepository<PageWidgetEntity> repo,
-      PageWidgetPredicateBuilder predicate) {
+      PageWidgetPredicateBuilder predicate,
+      PageWidgetAttributeService pageWidgetAttributeService) {
     super(repo, predicate);
+    
+    addService("attributes", pageWidgetAttributeService);
   }
 }
