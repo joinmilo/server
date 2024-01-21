54b87bc4325c287096e7890cb39e04403cfd5f23
DOOMED2WIN <timon-steinhauser@web.de>
2024-01-21 18:04:18 +0100

    bugs fixed

--- src/main/java/app/wooportal/server/features/contest/vote/ContestVoteService.java
+++ src/main/java/app/wooportal/server/features/contest/vote/ContestVoteService.java
@@ -1,7 +1,8 @@
 package app.wooportal.server.features.contest.vote;
 
 import org.springframework.stereotype.Service;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import app.wooportal.server.base.userContext.security.UserContextAuthorizationService;
 import app.wooportal.server.core.base.DataService;
 import app.wooportal.server.core.repository.DataRepository;
 
@@ -9,8 +10,23 @@ import app.wooportal.server.core.repository.DataRepository;
 public class ContestVoteService
     extends DataService<ContestVoteEntity, ContestVotePredicateBuilder> {
 
+  private final UserContextAuthorizationService authService;
+
   public ContestVoteService(DataRepository<ContestVoteEntity> repo,
-      ContestVotePredicateBuilder predicate) {
+      ContestVotePredicateBuilder predicate, UserContextAuthorizationService authService) {
     super(repo, predicate);
+
+    this.authService = authService;
+  }
+
+  @Override
+  public void preCreate(ContestVoteEntity entity, ContestVoteEntity newEntity, JsonNode context) {
+
+    var currentUser = authService.getAuthenticatedUserContext();
+
+    if (currentUser.isPresent()) {
+      newEntity.setUserContext(currentUser.get());
+      addContext("userContext", context);
+    } 
   }
 }
